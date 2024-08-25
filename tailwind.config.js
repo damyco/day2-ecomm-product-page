c2b5ba40772ce15f850dc0aebf572de08804a8dd
damyco <25842807+damyco@users.noreply.github.com>
2024-08-25 10:33:57 +0100

    add custom theme colours to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandblue: {
+          DEFAULT: "#1A75FF",
+          primary: "#1A75FF",
+          light: "#AACCFF",
+        },
+        brandgray: {
+          DEFAULT: "#D6D7DB",
+          light: "#D6D7DB",
+          medium: "#757374",
+          dark: "#646767",
+        },
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+      },
+    },
   },
   plugins: [],
 }
