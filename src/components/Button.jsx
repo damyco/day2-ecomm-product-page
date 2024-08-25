eabf3782c75a2f1804375072b0f02f6dc5bc5b45
damyco <25842807+damyco@users.noreply.github.com>
2024-08-25 10:35:24 +0100

    code format

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
-export default function Button({children, onClick}) {
+export default function Button({ children, onClick }) {
   return (
     <button
-    onClick={onClick}
-    className="bg-brandblue text-white w-full py-2 uppercase rounded mt-6 transition hover:bg-brandblue/90 hover:shadow-md"
-  >
-    {children}
-  </button>
-  )
+      onClick={onClick}
+      className="bg-brandblue text-white w-full py-2 uppercase rounded mt-6 transition hover:bg-brandblue/90 hover:shadow-md"
+    >
+      {children}
+    </button>
+  );
 }
